fd6d0c396323f00d78b95ee9f91f249bee88d1b9
Tyler Friedberg <thefriedberger@gmail.com>
2020-02-05 15:05:53 -0500

    Finished base, going to try to add more i.e. implement start and end screen, different difficulties, etc.

--- Assets/Scripts/HighScore.cs
+++ Assets/Scripts/HighScore.cs
@@ -7,9 +7,13 @@ public class HighScore : MonoBehaviour
 {
     static public int score = 1000;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        if(PlayerPrefs.HasKey("HighScore")) {
+            score = PlayerPrefs.GetInt("HighScore");
+        }
+
+        PlayerPrefs.SetInt("HighScore", score);
     }
 
     // Update is called once per frame
@@ -17,5 +21,8 @@ public class HighScore : MonoBehaviour
     {
         Text gt = this.GetComponent<Text>();
         gt.text = "High Score: " + score;
+        if(score > PlayerPrefs.GetInt("HighScore")) {
+            PlayerPrefs.SetInt("HighScore", score);
+        }
     }
 }
